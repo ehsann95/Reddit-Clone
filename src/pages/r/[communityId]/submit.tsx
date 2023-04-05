89bace224c12fbaa078a99eef333f1edbefcc560
Ahsan Nazir <psychoahsan@gmail.com>
2023-04-05 14:00:03 +0530

    Create Post Page and component.

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,8 +1,22 @@
+import PageContent from "@/src/components/Layout/PageContent";
+import NewPostForm from "@/src/components/Posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 type submitProps = {};
 
 const submit: React.FC<submitProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a Post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      {/* About section */}
+      <></>
+    </PageContent>
+  );
 };
 export default submit;
