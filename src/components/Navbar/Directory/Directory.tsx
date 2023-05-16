a5275625d084111c988dedadd638e3cb0fa86aae
Ahsan Nazir <psychoahsan@gmail.com>
2023-05-16 20:03:57 +0530

    useDirectory hook and atom.

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,39 +1,77 @@
+import useDirectory from "@/src/hooks/useDirectory";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Flex, Icon, Menu, MenuButton, MenuList, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 import { TiHome } from "react-icons/ti";
 import Communities from "./Communities";
 
 const Directory: React.FC = () => {
+  const { directoryState, toggleMenuOpen } = useDirectory();
+
   return (
-    <Menu>
-      <MenuButton
-        cursor="pointer"
-        padding="0px 6px"
-        borderRadius="4px"
-        mr={2}
-        ml={{ base: 0, md: 2 }}
-        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-      >
-        <Flex
-          alignItems="center"
-          justify="space-between"
-          width={{ base: "auto", lg: "200px" }}
-        >
-          <Flex alignItems="center">
-            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
-            <Flex display={{ base: "none", lg: "flex" }}>
-              <Text fontWeight={600} fontSize="10pt">
-                Home
-              </Text>
+    <Menu isOpen={directoryState.isOpen}>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            cursor="pointer"
+            padding="0px 6px"
+            borderRadius="4px"
+            _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+            mr={2}
+            ml={{ base: 0, md: 2 }}
+            onClick={toggleMenuOpen}
+          >
+            <Flex
+              alignItems="center"
+              justifyContent="space-between"
+              width={{ base: "auto", lg: "200px" }}
+            >
+              <Flex alignItems="center">
+                <>
+                  {directoryState.selectedMenuItem.imageURL ? (
+                    <Image
+                      alt=""
+                      borderRadius="full"
+                      boxSize="24px"
+                      src={directoryState.selectedMenuItem.imageURL}
+                      mr={2}
+                    />
+                  ) : (
+                    <Icon
+                      fontSize={24}
+                      mr={{ base: 1, md: 2 }}
+                      color={directoryState.selectedMenuItem.iconColor}
+                      as={directoryState.selectedMenuItem.icon}
+                    />
+                  )}
+                  <Box
+                    display={{ base: "none", lg: "flex" }}
+                    flexDirection="column"
+                    fontSize="10pt"
+                  >
+                    <Text fontWeight={600}>
+                      {directoryState.selectedMenuItem.displayText}
+                    </Text>
+                  </Box>
+                </>
+              </Flex>
+              <ChevronDownIcon color="gray.500" />
             </Flex>
-          </Flex>
-          <ChevronDownIcon color="gray.500" />
-        </Flex>
-      </MenuButton>
-      <MenuList>
-        <Communities />
-      </MenuList>
+          </MenuButton>
+          <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+            <Communities />
+          </MenuList>
+        </>
+      )}
     </Menu>
   );
 };
